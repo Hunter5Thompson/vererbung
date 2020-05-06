91441a7d98d0962a810b0e1e10727c25c9df896d
strosche <robert.scheike@student.hs-anhalt.de>
2020-05-07 01:38:49 +0200

    Das ist ein Test

---
+++ appli/vererbungstest/brillen.java
@@ -0,0 +1,40 @@
+package Package ;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class brillen extends entertainment products
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	protected boolean augmentet;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	protected boolean vr;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public brillen(){
+		super();
+	}
+
+}
+
