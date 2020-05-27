fa98c65f367ca1bde5e1b17eafc9df5feb84e56e
Hunter5Thompson <eistgleichmc-2@gmx.net>
2020-05-27 02:42:38 +0200

    Aenderung am Code entfernung der autogenerierten comments

--- vererbungsTest/kebap.java
+++ vererbungsTest/kebap.java
@@ -1,37 +1,14 @@
 package Package ;
 
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-
 public class kebap extends food
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+	
 	
 	protected boolean scharf;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	public boolean veggie;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
+	
 	public kebap(){
 		super();
 	}
